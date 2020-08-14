7006c03dd4311c52b19bb79d9d0c6853e4b12dac
Мещанкин Дмитрий <mityankin@gmail.com>
2020-08-14 12:06:17 +0300

    MySql
    Игровая площадка для тестов запросов к БД

--- task6-mysql/script.js
+++ task6-mysql/script.js
@@ -5,10 +5,23 @@ $('#requestForm').submit(function(e) {
     url: 'action.php',
     data: $(this).serialize(),
     success: function(response) {
-      element = $('<p></p>', {
+      element = $('<plaintext></plaintext>', {
         text: response
       });
       $('#requestForm').after(element);
     }
   });
+});
+
+$('#select_all').on('click', function(e) {
+  $('#input').val('select * from a');
+});
+$('#delete_last').on('click', function(e) {
+  $('#input').val('delete from a order by c1 desc limit 1');
+});
+$('#update_first').on('click', function(e) {
+  $('#input').val('update a set c2 = \'new_value\' where c1 = (select min(c1) from a)');
+});
+$('#create_table').on('click', function(e) {
+  $('#input').val('create table new_table (\n\tc1 int,\n\tc2 varchar(20)\n)');
 });
